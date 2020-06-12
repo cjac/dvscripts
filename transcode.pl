9b7c3332f8bb03897f8db0abf2d12ca877e0a275
C.J. Collier <cj.collier@shiftboard.com>
2020-06-12 09:22:38 -0700

    this one got er done

--- transcode.pl
+++ transcode.pl
@@ -8,7 +8,7 @@ my $DEBUG = 0;
 
 my $prefix = 'oscon2006';
 
-my( @infiles ) = glob("$prefix-*.dv");
+my( @infiles ) = sort glob("$prefix-*.dv");
 
 chomp(@infiles);
 
@@ -26,31 +26,46 @@ foreach my $infile( @infiles) {
       ("$prefix-${datetime}.mkv",
        "$prefix-${datetime}.mp4" );
 
+    my $dv_duration = qx(ffmpeg -i ${infile} 2>&1 | grep "Duration");
+    $dv_duration =~ s/^\s+(\S.+?\S)\s+$/$1/;
+
+    # Duration: 00:14:55.46, start: 0.000000, bitrate: 28771 kb/s
+    my %dv_duration = map {
+      split(': ', $_);
+    } split(', ', $dv_duration);
+
+    my @dv_duration = split( /[:\.]/, $dv_duration{Duration} );
+    my $dv_dur = DateTime::Duration->new(
+                                         hours       => $dv_duration[0],
+                                         minutes     => $dv_duration[1],
+                                         seconds     => $dv_duration[2],
+                                         nanoseconds => $dv_duration[3] * 100000,
+                                        );
+
+    my $dv_dt = $start_dt->clone->add_duration($dv_dur);
+
+    my $short_dv = 0;
+    if( $dv_dur->hours == 0
+        && $dv_dur->minutes == 0
+        && $dv_dur->seconds < 10
+      ){
+      $short_dv = 1;
+    }
+
+    my $mp4_path = '';
     if( -f "done/$mp4_file" ){
-      print("mp4 file exists$/") if $DEBUG;
-      my $dv_duration = qx(ffmpeg -i ${infile} 2>&1 | grep "Duration");
-      $dv_duration =~ s/^\s+(\S.+?\S)\s+$/$1/;
+      $mp4_path = "done/$mp4_file";
+    } elsif( "done/short/$mp4_file" ){
+      $mp4_path = "done/short/$mp4_file";
+    }
 
-      print(" dv: [$dv_duration]$/") if $DEBUG;
-      # Duration: 00:14:55.46, start: 0.000000, bitrate: 28771 kb/s
-      my %dv_duration = map {
-        split(': ', $_);
-      } split(', ', $dv_duration);
+    if( -f $mp4_path ){
+      print("mp4 file [$mp4_path] exists$/");
 
-      my @dv_duration = split( /[:\.]/, $dv_duration{Duration} );
-      my $dv_dur = DateTime::Duration->new(
-                                           hours       => $dv_duration[0],
-                                           minutes     => $dv_duration[1],
-                                           seconds     => $dv_duration[2],
-                                           nanoseconds => $dv_duration[3] * 100000,
-                                          );
-
-      my $dv_dt = $start_dt->clone->add_duration($dv_dur);
-
-      my $mp4_duration = qx(ffmpeg -i done/${mp4_file} 2>&1 | grep "Duration");
+      my $mp4_duration = qx(ffmpeg -i ${mp4_path} 2>&1 | grep "Duration");
       $mp4_duration =~ s/^\s+(\S.+?\S)\s+$/$1/;
 
-      print("mp4: [$mp4_duration]$/") if $DEBUG;
+      print("mp4: [$mp4_duration]$/");
       # Duration: 00:14:55.50, start: 0.000000, bitrate: 2269 kb/s
       my %mp4_duration = map {
         split(': ', $_);
@@ -68,17 +83,27 @@ foreach my $infile( @infiles) {
       my $mp4_dt = $start_dt->clone->add_duration($mp4_dur);
       my $stream_delta = abs( $dv_dt->epoch - $mp4_dt->epoch );
 
-      if( $dv_duration{Duration} eq $mp4_duration{Duration}
-        ){
-        print("duration of raw data matches duration of mp4.  Next.$/") if $DEBUG;
+      if( $dv_duration{Duration} eq $mp4_duration{Duration} ){
+        if ( $mp4_path !~ m{^done/short}
+             && $short_dv
+           ) {
+          print("moving short mp4 to done/short",$/);
+          qx(mv $mp4_path done/short);
+        }
         next;
       }elsif( $stream_delta < 5 ){
         print("delta between video lengths is $stream_delta seconds.  Next.$/") if $DEBUG;
+        if ( $mp4_path !~ m{^done/short}
+             && $short_dv
+           ) {
+          print("moving short mp4 [$mp4_file] to done/short",$/);
+          qx(mv $mp4_path done/short);
+        }
         next;
       }else{
         print("DV and mp4 data differ.  re-encoding$/") if $DEBUG;
+        unlink ${mp4_path};
       }
-
     }
 
     print($/,
@@ -86,17 +111,18 @@ foreach my $infile( @infiles) {
           "# infile: $infile",$/,
           "#",$/,
          );
+    print(" dv: [$dv_duration]$/");
     print("    datetime: $datetime$/") if $DEBUG;
 
     # make the completed directory if it doesn't exist
-    qx{mkdir -p done};
+    qx{mkdir -p done/short };
 
     # Store dv data in mkv container
-    print("Converting DV  [$infile] to mkv [${mkv_file}] @ ", scalar( localtime ), $/);
+    print("Converting DV  [$infile]  to mkv [${mkv_file}] @ ", scalar( localtime ), $/);
     my $cmd = qq{ffmpeg -threads 4 -i ${infile} -c:v copy -c:a copy ./${mkv_file}};
     $cmd .= ' 2>&1 > /dev/null' unless $DEBUG;
     my $output = qx($cmd);
-    print("done       DV  [$infile] to mkv [${mkv_file}] @ ", scalar( localtime ), $/);
+    print("done       DV  [$infile]  to mkv [${mkv_file}] @ ", scalar( localtime ), $/);
 
     # Thanks go to the #ffmpeg freenode channel for the following
     $cmd = 'ffmpeg'.
@@ -116,11 +142,16 @@ foreach my $infile( @infiles) {
     print("Converting mkv [${mkv_file}] to mp4 [${mp4_file}] @ ", scalar( localtime ), $/);
 
     $output = qx($cmd);
-    print("done       mkv [${mkv_file}] to mp4 [${mp4_file}]$/ @ ", scalar( localtime ), $/);
+    print("done       mkv [${mkv_file}] to mp4 [${mp4_file}] @ ", scalar( localtime ), $/);
 
     if( $? == 0 ){
-      print("moving mp4 file to done/$/") if $DEBUG;
-      $output = qx(mv ${mp4_file} done/);
+      if ( $short_dv ) {
+        print("moving short mp4 [$mp4_file] to done/short",$/);
+        qx(mv $mp4_file done/short);
+      }else{
+        print("moving mp4 [$mp4_file] to done/$/") if $DEBUG;
+        $output = qx(mv ${mp4_file} done/);
+      }
 
       print("Removing mkv file$/") if $DEBUG;
       $output = qx(rm ${mkv_file});
